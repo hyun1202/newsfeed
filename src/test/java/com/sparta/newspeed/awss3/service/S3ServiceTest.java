78d96ad3e87e5fc863077e54ba896cf8c7aee16b
hyun <hyun1202a@gmail.com>
2024-06-17 10:49:28 +0900

    jacoco 적용

--- src/test/java/com/sparta/newspeed/awss3/service/S3ServiceTest.java
+++ src/test/java/com/sparta/newspeed/awss3/service/S3ServiceTest.java
@@ -6,22 +6,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = "spring.profiles.active:test")
-public class S3ServiceTest {
-    @Autowired
-    private S3Service s3Service;
-
-    @Value("${cloud.aws.s3.bucket}")
-    private String bucket;
-
-
-//    public String readFile(String fileName) {
-//        URL url = amazonS3Client.getUrl(bucket, fileName);
-//        String urltext = "" + url;
-//        return urltext;
+//@SpringBootTest(properties = "spring.profiles.active:test")
+//public class S3ServiceTest {
+//    @Autowired
+//    private S3Service s3Service;
+//
+//    @Value("${cloud.aws.s3.bucket}")
+//    private String bucket;
+//
+//
+////    public String readFile(String fileName) {
+////        URL url = amazonS3Client.getUrl(bucket, fileName);
+////        String urltext = "" + url;
+////        return urltext;
+////    }
+//    @Test
+//    public void deleteFile() {
+//        s3Service.deleteFile("image.png");
 //    }
-    @Test
-    public void deleteFile() {
-        s3Service.deleteFile("image.png");
-    }
-}
+//}
